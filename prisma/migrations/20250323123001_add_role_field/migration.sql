d943176166f3ae54de3406e04970e877fe30691e
gabrieljuninho <gabrieltobing789@gmail.com>
2025-03-23 19:33:24 +0700

    feat(db): add role field to user model

---
+++ prisma/migrations/20250323123001_add_role_field/migration.sql
@@ -0,0 +1,5 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('SUPER_ADMIN', 'ADMIN', 'MODERATOR', 'MEMBER', 'VIEWER', 'GUEST');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'GUEST';
