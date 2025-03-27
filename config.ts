96e3dee9a108f79eb949e5aba1eab80a467c0b68
gabrieljuninho <gabrieltobing789@gmail.com>
2025-03-27 20:55:11 +0700

    chore: setup cloudinary

--- config.ts
+++ config.ts
@@ -1,1 +1,5 @@
 export const APP_URL = process.env.NEXT_PUBLIC_APP_URL!;
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME!;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY!;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET!;
